1349311cc0247a96a1a94c8578d2daef7e480587
RuymarAraujo <ruymararaujo@gmail.com>
2019-06-14 08:58:41 -0300

    Aplicação concluída !!!

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,11 +1,15 @@
 package com.londonappbrewery.climapm;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,13 +29,17 @@ import cz.msebera.android.httpclient.Header;
 public class WeatherController extends AppCompatActivity {
 
     // Constants:
-    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    final int REQUEST_CODE = 123;
+    final String WEATHER_URL =
+            "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+    final String APP_ID = "b89373e47071cd3ac2b8fe8b31e77bcd";
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
     final float MIN_DISTANCE = 1000;
+
+
 
     // TODO: Set LOCATION_PROVIDER here:
     String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
@@ -41,7 +49,6 @@ public class WeatherController extends AppCompatActivity {
 
     LocationManager mLocationManager;
     LocationListener mLocationListener;
-
 
 
     // Member Variables:
@@ -81,19 +88,25 @@ public class WeatherController extends AppCompatActivity {
         super.onResume();
         Log.d(LOGCAT_TAG, "onResume() called");
         Intent myIntent = getIntent();
-        String city = myIntent.getStringExtra( "City");
-        if(city!=null){
+        String city = myIntent.getStringExtra("City");
+        if (city != null) {
+            Log.d(LOGCAT_TAG, "Get new city: " + city);
             getWeatherForNewCity(city);
-        }else{
-            Log.d(LOGCAT_TAG,"Getting weather for current location");
+        } else {
+            Log.d(LOGCAT_TAG, "Getting weather for current location");
             getWeatherForCurrentLocation();
         }
     }
 
 
-
     // TODO: Add getWeatherForNewCity(String city) here:
+    private void getWeatherForNewCity(String city) {
+        RequestParams params = new RequestParams();
 
+        params.put("q", city);
+        params.put("appid",APP_ID);
+        letsDoSomeNetworking(params);
+    }
 
 
     // TODO: Add getWeatherForCurrentLocation() here:
@@ -102,17 +115,62 @@ public class WeatherController extends AppCompatActivity {
         mLocationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                Log.d("Clima", "onLocationChanged() callback received");
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+                RequestParams params = new RequestParams();
+                params.put("lat",latitude);
+                params.put("lon", longitude);
+                params.put("appid",APP_ID);
+                letsDoSomeNetworking(params);
             }
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle) {
+                Log.d(LOGCAT_TAG, "onStatusChanged() callback received. Status: " + i);
+                Log.d(LOGCAT_TAG, "2 means AVAILABLE, 1: TEMPORARILY_UNAVAILABLE, 0: OUT_OF_SERVICE");
             }
+
             @Override
             public void onProviderEnabled(String s) {
+                Log.d(LOGCAT_TAG, "onProviderEnabled() callback received. Provider: " + s);
             }
+
             @Override
             public void onProviderDisabled(String s) {
+                Log.d("Clima", "onProviderDisabled() callback received");
             }
         };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME,MIN_DISTANCE, mLocationListener);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull
+            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+// Checking against the request code we specified earlier.
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults. length > 0 && grantResults[ 0] == PackageManager. PERMISSION_GRANTED) {
+                Log. d(LOGCAT_TAG, "onRequestPermissionsResult(): Permission granted!");
+// Getting weather only if we were granted permission.
+                getWeatherForCurrentLocation();
+            } else {
+                Log. d(LOGCAT_TAG, "Permission denied =( ");
+            }
+        }
     }
 
 
